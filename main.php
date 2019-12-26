ac6227cdb7dc618f1cd46977f801e0aabf09f1ef
monikadedeh <monikadedeh1@gmail.com>
2019-12-26 17:12:20 +0700

    main

---
+++ main.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-light">
+    <div class="container"> <a class="navbar-brand text-primary" href="#">
+        <i class="fa d-inline fa-lg fa-stop-circle"></i>
+        <b><b> SPP Digital SMAN 1 Lohbener</b></b>
+      </a> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar4">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar4">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"> <a class="nav-link" href="main.php">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="https://www.google.com/maps/dir//Alamat+SMAN+1+Lohbener/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e6eb8624311efd1:0x27fcc8130d6152fd?sa=X&ved=2ahUKEwj35KeWlb_mAhUBfX0KHUgwBIIQ9RcwDHoECAsQEg">Alamat</a> </li>
+        </ul> <a class="btn navbar-btn ml-md-2 btn-light" href="Logout.php"><i class="fa fa-user fa-fw"></i>
+          <font face="FontAwesome">Log Out</font>
+        </a>
+      </div>
+    </div>
+  </nav>
+  <div class="py-5 text-center bg-info" >
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 bg-primary">
+          <h1 class="bg-light text-primary"><span class="text-left text-capitalize"><b>Berikut adalah rincian pembayaran SPP Anda :</b></span></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-primary">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="1.png" alt="Bulan Januari">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Januari</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Januari</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="2.png" alt="Bulan Februari">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Februari</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Februari</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+           <div class="card"> <img class="card-img-top" src="3.png" alt="Bulan Maret">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Maret</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Maret</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="4.png" alt="Bulan April">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan April</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan April</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="5.png" alt="Bulan Mei">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Mei</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Mei</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="6.png" alt="Bulan Juni">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Juni</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Juni</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="7.png" alt="Bulan Juli">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Juli</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Juli</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="8.png" alt="Bulan Agustus">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Agustus</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Agustus</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+  </div>
+  <div class="py-5 bg-primary">
+    <div class="container">
+      <div class="row">
+       <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="9.png" alt="Bulan September">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan September</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan September</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="10.png" alt="Bulan Oktober">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan <br>Oktober</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Oktober</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="11.png" alt="Bulan November">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan November</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan November</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card"> <img class="card-img-top" src="12.png" alt="Bulan Desember">
+            <div class="card-body">
+              <h4 class="card-title">SPP Bulan Desember</h4>
+              <p class="card-text">Silahkan upload bukti pembayaran Anda untuk konfirmasi pembayaran SPP untuk bulan Desember</p> <a href="form.php" class="btn btn-primary">Upload</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-6 p-3">
+          <h5> <b>Main</b> </h5>
+          <ul class="list-unstyled">
+            <li> <a href="main.php">Home</a> </li>
+            <li> <a href="https://www.google.com/maps/dir//Alamat+SMAN+1+Lohbener/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e6eb8624311efd1:0x27fcc8130d6152fd?sa=X&ved=2ahUKEwj35KeWlb_mAhUBfX0KHUgwBIIQ9RcwDHoECAsQEg">Alamat</a> </li>
+          </ul>
+        </div>
+       <div class="col-lg-3 col-md-6 p-3">
+          <h5> <b>About</b> </h5>
+          <p class="mb-0"> SMA Negeri 1 Lohbener adalah salah satu Sekolah Menengah Atas yang berada di Kecamatan Lohbener Kabupaten Indramayu</p>
+        </div>
+        <div class="col-lg-3 col-md-6 p-3">
+          <h5> <b>Follow us</b> </h5>
+          <div class="row">
+            <div class="col-md-12 d-flex align-items-center justify-content-between my-2"> 
+                <a href="https://www.facebook.com/SMAN-1-Lohbener-1177160618979516/?ref=py_c">
+                <i class="d-block fa fa-facebook-official text-muted fa-lg mr-2"></i>
+                </a> 
+                <a href="https://www.instagram.com/smansaloh/">
+                <i class="d-block fa fa-instagram text-muted fa-lg mx-2"></i>
+                </a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p class="mb-0 mt-2">© 2019 SMA Negeri 1 Lohbener</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
